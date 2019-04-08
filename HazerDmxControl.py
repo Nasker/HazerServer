eebdd4e2dd4e30495a40970751d71f975863edb2
Nasker <xamps23@gmail.com>
2019-04-08 10:58:43 +0200

    First commit with source files

---
+++ HazerDmxControl.py
@@ -0,0 +1,22 @@
+from DmxPy import DmxPy 
+
+class HazerDmxControl:
+    def __init__(self, serialPort):
+        self.dmx = DmxPy(serialPort)
+        self.hazeChannel = 1
+        self.fanChannel = 2
+        
+    
+    def _percentToDMXRange(self, percentage):
+        return int(round( (percentage / 100.0) * 255))
+        
+    def _calcAndSend(self, channel, percentage):
+        self.dmx.setChannel(channel, self._percentToDMXRange(percentage))
+        self.dmx.render()   
+        
+    def setHazeIntensity(self, hazePercentage):
+        self._calcAndSend(self.hazeChannel, hazePercentage)
+        
+    def setFanSpeed(self, fanPercentage):
+        self._calcAndSend(self.fanChannel, fanPercentage)
+        
---
+++ HazerServer.py
@@ -0,0 +1,55 @@
+import OSC
+import sys
+from multiprocessing import Process
+from time import sleep
+from HazerDmxControl import HazerDmxControl
+
+server = OSC.OSCServer( ("0.0.0.0", 9000) )
+server.timeout = 0
+run = True
+
+dmxControl = HazerDmxControl("/dev/ttyUSB0")
+
+def handleTimeout(self):
+    self.timed_out = True
+
+# funny python's way to add a method to an instance of a class
+import types
+server.handleTimeout = types.MethodType(handleTimeout, server)
+
+def userCallback(path, tags, args, source):
+    print ("-Receiving: "+path+"\t"+tags+"\t",args)
+    
+    
+def hazeCallback(path, tags, args, source):
+    print "-Receiving: "+path+"\t"+tags+"\t",args[0], args[1]
+    dmxControl.setHazeIntensity(50)
+    dmxControl.setFanSpeed(50)
+
+def quitCallback(path, tags, args, source):
+    # don't do this at home (or it'll quit blender)
+    global run
+    run = False
+
+server.addMsgHandler( "/haze/set", hazeCallback )
+server.addMsgHandler( "/quit", quitCallback )
+
+def eachFrame():
+    print("Starting Server!")
+    while run:
+    # clear timed_out flag
+        sleep(0.1)
+        server.timed_out = False
+        # handle all pending requests then return
+        while not server.timed_out:
+            server.handle_request()
+
+try:
+    eachFrameThread = Process(target=eachFrame)
+    #readnSendCCSPrimaThread = Process(target=readnSendCCSPrima)
+    eachFrameThread.start()
+    #readnSendCCSPrimaThread.start()
+    eachFrameThread.join()
+    #readnSendCCSPrimaThread.join()
+except(KeyboardInterrupt, SystemExit): 
+    server.close()
